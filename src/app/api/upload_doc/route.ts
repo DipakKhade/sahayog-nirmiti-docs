3152817a6132890511614305e3010d9b50f58374
Dipak Khade <dipakhade214@gmail.com>
2025-09-14 20:19:53 +0530

    added file save api

---
+++ src/app/api/upload_doc/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+
+export async function POST(req: NextRequest) {
+    const formData = await req.formData();
+  const file = formData.get("file") as File;
+  console.log(file)
+
+  if (!file) {
+    return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const uploadDir = path.join(process.cwd(), "uploads");
+  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+
+  const filePath = path.join(uploadDir, file.name);
+  fs.writeFileSync(filePath, buffer);
+
+  return NextResponse.json({ success: true, fileName: file.name });
+}
