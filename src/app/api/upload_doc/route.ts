28df809668afd0bae27fe5e1293c26756c3a659e
Dipak Khade <dipakhade214@gmail.com>
2025-09-15 20:54:12 +0530

    rm log

--- src/app/api/upload_doc/route.ts
+++ src/app/api/upload_doc/route.ts
@@ -4,21 +4,20 @@ import path from "path";
 
 export async function POST(req: NextRequest) {
     const formData = await req.formData();
-  const file = formData.get("file") as File;
-  console.log(file)
+    const file = formData.get("file") as File;
 
-  if (!file) {
-    return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
-  }
+    if (!file) {
+        return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+    }
 
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-  const uploadDir = path.join(process.cwd(), "uploads");
-  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+    const uploadDir = path.join(process.cwd(), "uploads");
+    if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
-  const filePath = path.join(uploadDir, file.name);
-  fs.writeFileSync(filePath, buffer);
+    const filePath = path.join(uploadDir, file.name);
+    fs.writeFileSync(filePath, buffer);
 
-  return NextResponse.json({ success: true, fileName: file.name });
+    return NextResponse.json({ success: true, fileName: file.name });
 }
