6bb1e7f90bacc21de6dbbe1a97474cc38d1b377e
Dipak Khade <dipakhade214@gmail.com>
2025-09-18 16:25:05 +0530

    added types ts

---
+++ src/lib/types.ts
@@ -0,0 +1,59 @@
+export interface Document {
+    id: number
+    invoiceNo: string
+    purchaseOrderNo: string
+    partNo: string
+    partName: string
+    documentType: string
+    userId: number
+    bucketKey: string
+    createdAt: string
+    user: {
+      username: string
+      vendorCode: string | null
+    }
+  }
+  
+  export interface GroupedInvoice {
+    invoiceNo: string
+    purchaseOrderNo: string
+    partNo: string
+    partName: string
+    documentType: string
+    supplierName: string
+    vendorCode: string | null
+    documents: Document[]
+  }
+  
+  export interface PaginationData {
+    currentPage: number
+    totalPages: number
+    totalCount: number
+    hasNext: boolean
+    hasPrev: boolean
+  }
+  
+  export interface SuppliersTableProps {
+    documents: Document[]
+    loading: boolean
+    pagination: PaginationData
+    onViewDocuments: (documents: any) => void
+    onPageChange: (page: number) => void
+  }
+  
+
+  export interface DocumentData {
+    id: number
+    invoiceNo: string
+    purchaseOrderNo: string
+    partNo: string
+    partName: string
+    documentType: string
+    userId: number
+    bucketKey: string
+    createdAt: string
+    user: {
+      username: string
+      vendorCode: string | null
+    }
+  }
