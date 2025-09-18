b505960d692fee24ab89a0cb5f20a9b308f07dd8
Dipak Khade <dipakhade214@gmail.com>
2025-09-18 14:28:16 +0530

    storing file in fe to upload to later

--- src/app/api/upload_doc/route.ts
+++ src/app/api/upload_doc/route.ts
@@ -1,24 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
+import { v4 } from 'uuid';
+import { uploadFilesToS3 } from "@/lib/s3";
 
 export async function POST(req: NextRequest) {
     const formData = await req.formData();
     const file = formData.get("file") as File;
-
+    
     if (!file) {
         return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
     }
 
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
+    try {
+        const uploadDir = path.join(process.cwd(), `uploads/ ${v4()}`);
+        if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+        
+        const filePath = path.join(uploadDir, file.name);
+        console.log('filePath', filePath)
+        
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+        
+        fs.writeFileSync(filePath, buffer);
+        
+        await uploadFilesToS3([filePath]);
 
-    const uploadDir = path.join(process.cwd(), "uploads");
-    if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+        return NextResponse.json({ success: true, fileName: file.name });
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json({ success: false });
+    }
 
-    const filePath = path.join(uploadDir, file.name);
-    console.log('filePath', filePath)
-    fs.writeFileSync(filePath, buffer);
 
-    return NextResponse.json({ success: true, fileName: file.name });
+
 }
