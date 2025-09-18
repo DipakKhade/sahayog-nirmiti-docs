d3ccaf9332aa8bea778102c83389c95147307b1b
Dipak Khade <dipakhade214@gmail.com>
2025-09-18 15:19:41 +0530

    getting other form details

--- src/app/api/upload_doc/route.ts
+++ src/app/api/upload_doc/route.ts
@@ -3,17 +3,37 @@ import fs from "fs";
 import path from "path";
 import { v4 } from 'uuid';
 import { uploadFilesToS3 } from "@/lib/s3";
+import { cookies } from 'next/headers'
+import jwt from 'jsonwebtoken';
+import prisma from "@/lib/db";
 
 export async function POST(req: NextRequest) {
     const formData = await req.formData();
     const file = formData.get("file") as File;
-    
-    if (!file) {
+    const invoiceNo = formData.get("invoiceNo") as string;
+    const purchaseOrderNo = formData.get("purchaseOrderNo") as string;
+    const partNo = formData.get("partNo") as string;
+    const partName = formData.get("partName") as string;
+    const documentType = formData.get("documentType") as string;
+
+    const cookieStore = await cookies()
+    const token = cookieStore.get('token')?.value
+    if(!token) {
+        return NextResponse.json({
+            success: false,
+            message:"unAuthorized"
+        })
+    }
+    const payload = jwt.verify(token, process.env.JWT_SEC!) as {user_id: any}
+    const userId = payload.user_id
+
+    if (!file || !invoiceNo || !purchaseOrderNo || !partName || !partNo || !documentType) {
         return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
     }
 
     try {
-        const uploadDir = path.join(process.cwd(), `uploads/ ${v4()}`);
+        const dirKey = v4();
+        const uploadDir = path.join(process.cwd(), `uploads/ ${dirKey}`);
         if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
         
         const filePath = path.join(uploadDir, file.name);
@@ -23,15 +43,31 @@ export async function POST(req: NextRequest) {
         const buffer = Buffer.from(bytes);
         
         fs.writeFileSync(filePath, buffer);
-        
-        await uploadFilesToS3([filePath]);
+
+        const is_files_uploaded = await uploadFilesToS3([filePath]);
+
+        if(!is_files_uploaded){
+            return NextResponse.json({ success: false });
+        }
+
+        const r =  prisma.$transaction(async tx => {
+            const add_doc = await tx.document.create({
+                data :{
+                    documentType,
+                    invoiceNo,
+                    partName,
+                    partNo,
+                    purchaseOrderNo,
+                    userId,
+                    bucketKey: dirKey
+                }
+            })
+        })
 
         return NextResponse.json({ success: true, fileName: file.name });
     } catch (err) {
         console.log(err)
         return NextResponse.json({ success: false });
     }
-
-
 
 }
