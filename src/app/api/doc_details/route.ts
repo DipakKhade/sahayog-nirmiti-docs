ae98878eb8f08c0fb851fdd48d49fbe6296e93cb
Dipak Khade <dipakhade214@gmail.com>
2025-09-18 18:22:53 +0530

    added pagination data and where clause

--- src/app/api/doc_details/route.ts
+++ src/app/api/doc_details/route.ts
@@ -1,5 +1,7 @@
+import { perPageRows } from "@/lib/config";
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import { subMonths } from "date-fns"
 
 export async function GET(req: NextRequest) {
     let page = req.nextUrl.searchParams.get('page');
@@ -12,21 +14,51 @@ export async function GET(req: NextRequest) {
     }
 
     try {
-        const data = await prisma.document.findMany({
-            include : {
-                user: {
-                    select: {
-                        username: true,
-                        vendorCode: true
+
+        const data = await prisma.$transaction(async tx => {
+            const data = await tx.document.findMany({
+                include : {
+                    user: {
+                        select: {
+                            // username: true,
+                            vendorCode: true,
+                            name: true
+                        }
+                    }
+                },
+                orderBy: {
+                    user: {
+                        vendorCode: "asc"
+                    }
+                },
+                take: perPageRows,
+                where: {
+                    createdAt : {
+                        gte: subMonths(new Date(), 8)
                     }
                 }
+            })
+
+            const total_docs  = Math.ceil((await tx.document.count())/perPageRows);
+            const total_pages = total_docs / +page 
+            const pagination_data = {
+                totalPages: total_pages,
+                hasNext: +page < total_pages,
+                hasPrev: +page > 1,
+                totalCount: await tx.document.count()
             }
+
+            return {data, pagination_data}
+
         })
+       
+
 
         return NextResponse.json({
             success: true,
             message: "data retrived successfully",
-            data
+            data: data.data,
+            pagination_data: data.pagination_data
         })
     } catch(err){
         console.log(err)
